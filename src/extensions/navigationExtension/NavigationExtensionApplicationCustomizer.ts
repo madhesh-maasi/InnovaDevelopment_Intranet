21d9ef249334a8567101890705cd9a0e9975e4ca
madhesh-maasi <madheshmaasi3@gmail.com>
2025-07-10 22:43:01 +0530

    Maasi Extension guidace update 10/07/2025 22:45

--- src/extensions/navigationExtension/NavigationExtensionApplicationCustomizer.ts
+++ src/extensions/navigationExtension/NavigationExtensionApplicationCustomizer.ts
@@ -1,13 +1,11 @@
-import { Log } from '@microsoft/sp-core-library';
-import {
-  BaseApplicationCustomizer
-} from '@microsoft/sp-application-base';
-import { Dialog } from '@microsoft/sp-dialog';
+import { Log } from "@microsoft/sp-core-library";
+import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+// import { Dialog } from "@microsoft/sp-dialog";
 
-import * as strings from 'NavigationExtensionApplicationCustomizerStrings';
+import * as strings from "NavigationExtensionApplicationCustomizerStrings";
 
-const LOG_SOURCE: string = 'NavigationExtensionApplicationCustomizer';
-
+const LOG_SOURCE: string = "NavigationExtensionApplicationCustomizer";
+import "./ExtensionStyle.css";
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
  * it will be deserialized into the BaseExtension.properties object.
@@ -19,20 +17,19 @@ export interface INavigationExtensionApplicationCustomizerProperties {
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
-export default class NavigationExtensionApplicationCustomizer
-  extends BaseApplicationCustomizer<INavigationExtensionApplicationCustomizerProperties> {
-
+export default class NavigationExtensionApplicationCustomizer extends BaseApplicationCustomizer<INavigationExtensionApplicationCustomizerProperties> {
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
-    }
+    // let message: string = this.properties.testMessage;
+    // if (!message) {
+    //   message = "(No properties were provided.)";
+    // }
 
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
-      /* handle error */
-    });
+    //  Place your js logics here!!!
+    // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
+    //   /* handle error */
+    // });
 
     return Promise.resolve();
   }
