baace51d8666cfa40d0f1ef6720efcf0fbadb96a
Yokeshraj2001 <yokeshraj97.yr@gmail.com>
2025-07-10 21:50:16 +0530

    10-07-2025/minor changes in designs and extension added

---
+++ src/extensions/navigationExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface INavigationExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'NavigationExtensionApplicationCustomizerStrings' {
+  const strings: INavigationExtensionApplicationCustomizerStrings;
+  export = strings;
+}
